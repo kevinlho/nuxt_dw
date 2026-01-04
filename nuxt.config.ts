64f519f887cd2c3888c13830ee634a51129e3319
kevin.susanto <kevin.susanto@rupiahcepat.co.id>
2026-01-04 17:25:10 +0700

    github action ghpages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
+  ssr: false, // Static site generation
+  app: {
+    baseURL: '/nuxt_dw/', // e.g. '/my-nuxt-app/'
+    buildAssetsDir: 'assets',
+  },
+  // Ensure the build output directory is configured correctly
+  nitro: {
+    preset: 'static'
+  },
+  
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
